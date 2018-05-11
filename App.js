b82cad04dac16b7661751021e5380280db04b835
Teddy Ghim <teddyghim@gmail.com>
2018-05-11 02:42:24 +0200

    Hooking the weather data

--- App.js
+++ App.js
@@ -2,18 +2,20 @@ import React from 'react';
 import { StyleSheet, Text, View, Image, ActivityIndicator, StatusBar } from 'react-native';
 import Weather from "./Weather";
 
+const API_KEY = "bd3c0c97f0d8f2875b3e187b3cca452e";
+
 export default class App extends React.Component {
   state = {
     isLoaded: false,
-    error: null
+    error: null,
+    temperature:null,
+    name:null
   }
 
   componentDidMount(){
     navigator.geolocation.getCurrentPosition(
       position => {
-        this.setState({
-          isLoaded: true
-        });
+        this._getWeather(position.coords.latitude, position.coords.longitude)
       },
       error => { 
         this.setState({
@@ -22,6 +24,19 @@ export default class App extends React.Component {
       }
     );
   }
+  _getWeather = (lat,lon) => {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&APPID=${API_KEY}`)
+    .then(response => response.json())
+    .then(json => {
+      console.log(json);
+      this.setState({
+        temperature: json.main.temp,
+        name: json.weather[0].main,
+        isLoaded: true
+      });
+     })
+  }
+  
   render() {
     const { isLoaded, error } = this.state;
     return (
